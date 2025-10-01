21016f72239be5b79c58b86a047c931066ee26e3
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-10-01 10:56:32 +0530

    patterns

--- 02_WarmUp/04_secondLargest.js
+++ 02_WarmUp/04_secondLargest.js
@@ -1,1 +1,16 @@
- 
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     console.log("Loop", i, j);
+//   }
+// }
+
+
+for(let i=0;i<3;i++){
+    for(let j=0;j<=i;j++){
+        console.log(i,j);
+    }
+}
+
+// 0  0
+// 1  0
+// 2  1
